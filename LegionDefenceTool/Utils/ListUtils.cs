d2019c111b4fe04e49a0b8d6ca8352e1379bfdf9
James Wilkinson <jw@jameswilko.com>
2016-01-03 01:19:56 +1100

    Added ListUtils

---
+++ LegionDefenceTool/Utils/ListUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegionDefenceTool.Utils
+{
+	public static class ListUtils
+	{
+		public static string PrintList<T>(this List<T> list)
+		{
+			StringBuilder Builder = new StringBuilder();
+			foreach (T element in list)
+			{
+				if (Builder.Length > 0)
+				{
+					Builder.Append(", ");
+				}
+				Builder.Append(element);
+			}
+			return Builder.ToString();
+		}
+	}
+}
