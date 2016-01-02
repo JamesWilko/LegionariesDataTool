c9bc2c6f2799d78b8338e58e046bebb57af68e01
James Wilkinson <jw@jameswilko.com>
2016-01-02 21:16:34 +1100

    Unit generator adds all units to a generated file list and exports them to npc_units_custom

--- LegionDefenceTool/Generators/UnitFileGenerator.cs
+++ LegionDefenceTool/Generators/UnitFileGenerator.cs
@@ -11,22 +11,37 @@ namespace LegionDefenceTool.Generators
 {
 	public class UnitFileGenerator
 	{
-		const string TEMPLATE_FILE = "templates/npcs/npc_legion_unit_template.txt";
-		const string OUTPUT_FILE = "output/{0}.txt";
+		const string UNIT_TEMPLATE_FILE = "templates/npcs/npc_legion_unit_template.txt";
 		const string TEMPLATE_VARIABLE = "{{Unit.{0}}}";
+
+		const string UNIT_OUTPUT_TEMPLATE = "templates/npc_units_custom_template.txt";
+		const string UNIT_OUTPUT = "output/npc_units_custom.txt";
+
+		static string[] EXTERNAL_UNITS = new string[]
+		{
+			"data/npc_king_dire.txt",
+			"data/npc_king_radiant.txt",
+			"data/npc_miner.txt"
+		};
 
 		string UnitTemplateFile = "";
 
 		public void Generate(LegionDatabase Database)
 		{
-			if (File.Exists(TEMPLATE_FILE))
+			TextReader Reader;
+
+			if (File.Exists(UNIT_TEMPLATE_FILE))
 			{
 				// Load template file
-				TextReader Reader = new StreamReader(TEMPLATE_FILE, Encoding.UTF8);
+				Reader = new StreamReader(UNIT_TEMPLATE_FILE, Encoding.UTF8);
 				UnitTemplateFile = Reader.ReadToEnd();
 				Reader.Close();
 			}
 
+			// Units data
+			List<string> GeneratedUnitData = new List<string>();
+
+			// Build unit data for all units
 			foreach (LegionUnit Unit in Database.LegionUnits)
 			{
 				// Template file
@@ -81,12 +96,41 @@ namespace LegionDefenceTool.Generators
 					}
                 }
 
-				// Save file
-				string OutputPath = string.Format(OUTPUT_FILE, Unit.ID);
-				TextWriter Writer = new StreamWriter(OutputPath, false, Encoding.UTF8);
-				Writer.Write(Template);
-				Writer.Close();
+				// Add generated data
+				GeneratedUnitData.Add(Template);
+            }
+
+			// Add data from external unit files
+			foreach(string ExternalPath in EXTERNAL_UNITS)
+			{
+				// Read file data
+				Reader = new StreamReader(ExternalPath, Encoding.UTF8);
+				string UnitData = Reader.ReadToEnd();
+				Reader.Close();
+
+				// Add to generated data
+				GeneratedUnitData.Add(UnitData);
 			}
+
+			// Read template file
+			Reader = new StreamReader(UNIT_OUTPUT_TEMPLATE, Encoding.UTF8);
+			string TemplateFileData = Reader.ReadToEnd();
+			Reader.Close();
+
+			// Build file data
+			StringBuilder Builder = new StringBuilder();
+			foreach (var GeneratedUnit in GeneratedUnitData)
+			{
+				Builder.AppendLine(GeneratedUnit);
+			}
+
+			// Replace
+			TemplateFileData = TemplateFileData.Replace("{Units}", Builder.ToString());
+
+			// Write file
+			TextWriter Writer = new StreamWriter(UNIT_OUTPUT, false, Encoding.UTF8);
+			Writer.Write(TemplateFileData);
+			Writer.Close();
 		}
 
 	}
