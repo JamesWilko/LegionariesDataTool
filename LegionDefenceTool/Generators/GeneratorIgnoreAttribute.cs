e3abaef0e8aaeea9c35a4906c418313f427db1be
James Wilkinson <jw@jameswilko.com>
2016-01-02 17:46:41 +1100

    Added generator ignore attribute Used to specify fields and properties which should not be attempted to be added to a template file

---
+++ LegionDefenceTool/Generators/GeneratorIgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegionDefenceTool.Generators
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class GeneratorIgnore : Attribute
+	{
+	}
+}
